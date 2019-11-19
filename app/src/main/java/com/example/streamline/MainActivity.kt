00cb1eb16a3a171b3dd3fa3740f83cf93ab06113
ashwinxkumar01 <ashwinxkumar01@gmail.com>
2019-11-18 18:33:12 -0800

    xml changes

---
+++ app/src/main/java/com/example/streamline/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.streamline
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.functions.FirebaseFunctions
+import com.google.firebase.functions.FirebaseFunctionsException
+import org.json.JSONArray
+import org.json.JSONObject
+
+
+const val PARTY_ID = "party-id"
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+//        //Passing each menu ID as a set of Ids because each
+//        //menu should be considered as top level destinations.
+//        val appBarConfiguration = AppBarConfiguration(
+//            setOf(
+//                R.id.navigation_create, R.id.navigation_join
+//            )
+//        )
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+
+    }
+}
---
+++ app/src/main/java/com/example/streamline/ui/join/JoinViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.streamline.ui.join
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class JoinViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "Cosmix"
+    }
+    val text: LiveData<String> = _text
+}
